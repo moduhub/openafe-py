caed6bf757056ccb3fd160a9471d2d35613bbc54
ig-66 <igor@moduhub.com>
2023-08-14 16:36:29 -0300

    Added checksum calculation and refactored the main of the script

--- openafe_plotter.py
+++ openafe_plotter.py
@@ -4,19 +4,73 @@ import serial
 
 
 def getMessageFromOpenAFE():
+	"""
+	The function `getMessageFromOpenAFE` reads a message from a serial port, checks its checksum, and
+	returns the message if the checksum is valid, otherwise it returns -1.
+	:return: either the message received from OpenAFE if the checksum is valid, or -1 if the checksum is
+	not valid.
+	"""
 	messageReceived=str(ser.readline())
 	rawMessage = messageReceived[2:][:-5]
-	print(rawMessage)
+	
 	# check the message's checksum ...
-	isMessageOk = True
-	if isMessageOk:
+	calculatedChecksum = calculateChecksumOfString(messageReceived[3:][:-8])
+	checksumInMessage = getChecksumIntegerFromString(rawMessage[-2:])
+
+	if (calculatedChecksum - checksumInMessage) == 0:
+		# checksum is valid
 		message = messageReceived[3:][:-8] 
 		return message
 	else :
+		# checksum is not valid
 		return -1
 
 
+def calculateChecksumOfString(string):
+	"""
+	The function `calculateChecksumOfString` calculates the checksum of a given string by performing a
+	bitwise XOR operation on the ASCII values of its characters.
+	
+	:param string: The parameter "string" is a string of characters for which we want to calculate the
+	checksum, e.g.: "CVW,500,-500,250,2,1".
+	:return: the checksum of the given string, e.g.: 119 (0x77).
+	"""
+	checksum = 0
+	for char in string:
+		checksum = (checksum ^ ord(char))
+	return checksum
+
+
+def getChecksumIntegerFromString(checksumString):
+	"""
+	The function `getChecksumIntegerFromString` converts a string representation of a checksum into an
+	integer value.
+	
+	:param checksumString: The `checksumString` parameter is a string that represents a checksum, e.g.: "32".
+	:return: an integer checksum value e.g.: 50 (0x32).
+	"""
+	integerChecksum = 0
+	index = 1
+
+	for char in checksumString:
+		if ord(char) < 0x41:
+			integerChecksum |= (ord(char) - 0x30) << (index * 4)
+		else:
+			integerChecksum |= (ord(char) - 0x37) << (index * 4)
+		index -= 1
+
+	return integerChecksum
+
+
 def plotPoints(queVoltage, queCurrent):
+	"""
+	The function "plotPoints" plots points on a graph and sets the y-axis range.
+	
+	:param queVoltage: The queVoltage parameter represents the list of voltage values that you want to
+	plot on the x-axis. Each value in the list corresponds to a point on the plot
+	:param queCurrent: The queCurrent parameter represents the list of current values that you want to
+	plot on the y-axis. Each value in the list corresponds to a specific point on the plot
+	"""
 	# PLOTTING THE POINTS
 	plt.clf()
 	plt.plot(queVoltage, queCurrent)
@@ -29,15 +83,11 @@ def plotPoints(queVoltage, queCurrent):
 	plt.pause(0.1)
 
 
-fig, ax = plt.subplots()
-line, = ax.plot([], [], 'o-')  # Create an empty line with markers ('o-')
+# ***** ***** ***** MAIN ***** ***** *****:
 
 # MAX NO. OF POINTS TO STORE
 queVoltage = deque(maxlen = 2000)
 queCurrent = deque(maxlen = 2000)
-
-# Set the x and y data for the line
-line.set_data(queVoltage, queCurrent)
 
 ser = serial.Serial('COM6', 115200)
 
@@ -47,15 +97,12 @@ plt.xlim(-600,600)
 
 while True:
 	messageReceived = getMessageFromOpenAFE()
-	print(messageReceived)
 
 	if messageReceived == -1:
 		print("*** ERROR: Message corrupted!")
 		break
 
 	elif messageReceived == "MSG,RDY":
-		print('devide is ready!')
-		
 		# ser.write(b"$CVW,500,-500,250,10,2*47")
 		ser.write(b"$CVW,500,-500,250,2,1*77")
 
@@ -64,24 +111,25 @@ while True:
 		while messageReceived != "MSG,END":
 			messageReceived = getMessageFromOpenAFE()
 			point = messageReceived[4:] 
-			print(messageReceived)
+			print(messageReceived[4:])
 			
 			if messageReceived == "MSG,END":
 				plotPoints(queVoltage, queCurrent)
 				plt.pause(60)
 				break
 
-			pointObjs = point.split(',')
+			elif messageReceived != -1: # if message is valid
+				pointObjs = point.split(',')
 
-			voltage = float(pointObjs[0])
-			current = float(pointObjs[1])
+				voltage = float(pointObjs[0])
+				current = float(pointObjs[1])
 
-			queVoltage.append(voltage)
-			queCurrent.append(current)
+				queVoltage.append(voltage)
+				queCurrent.append(current)
 
-			if len(queVoltage) % 10 == 0: 
-				plotPoints(queVoltage, queCurrent)
-
+				if len(queVoltage) % 20 == 0: 
+					plotPoints(queVoltage, queCurrent)
+			
 		break
 
 print("finished! UHUL!!!!") 
